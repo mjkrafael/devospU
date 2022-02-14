50efe2fcf4cd5df97120158fcd63af91b941fef7
mjkrafael <mjkrafael@gmail.com>
2022-02-14 13:24:06 -0300

    add test -2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -14,7 +14,7 @@ public class BasicApplicationTests {
 	@Test
 	public void contextLoads() {
 		String meessage = "Default message cambio 11";
-		Assert.assertNotNull(message);
+		Assert.assertNotNull(meessage);
 	}
 
 }
